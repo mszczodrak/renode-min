bee439687c66048fef54746d9cf686dca22d33cd
mszczodrak@gmail.com <mszczodrak@gmail.com>
2023-03-10 17:06:33 -0800

    cleanup UI

--- src/Renode/Program.cs
+++ src/Renode/Program.cs
@@ -1,11 +1,12 @@
 using System.Threading;
-using Antmicro.Renode;
-using Antmicro.Renode.UI;
-using System.IO;
 using System;
 using Antmicro.Renode.Utilities;
-using Antmicro.Renode.RobotFramework;
 using Antmicro.Renode.Logging;
+using Antmicro.Renode.Logging.Backends;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.UserInterface;
+using AntShell;
+using AntShell.Terminal;
 
 namespace Antmicro.Renode
 {
@@ -23,7 +24,7 @@ namespace Antmicro.Renode
             {
                 try
                 {
-                    Antmicro.Renode.UI.CommandLineInterface.Run();
+                    RunMe();
                 }
                 finally
                 {
@@ -35,10 +36,55 @@ namespace Antmicro.Renode
         }
 
 
+        public static void RunMe()
+        {
+            Console.Out.WriteLine("HELLO!");
 
+            Console.Out.WriteLine("Console!");
+            Logger.AddBackend(ConsoleBackend.Instance, "console");
 
+            Logger.AddBackend(new MemoryBackend(), "memory");
+            Emulator.ShowAnalyzers = true;
 
+            using(var context = ObjectCreator.Instance.OpenContext())
+            {
+                var monitor = new Antmicro.Renode.UserInterface.Monitor();
+                context.RegisterSurrogate(typeof(Antmicro.Renode.UserInterface.Monitor), monitor);
 
-        
+                Shell shell = null;
+
+                Console.Out.WriteLine("Shell on port!");
+                var io = new IOProvider()
+                {
+                    Backend = new SocketIOSource(1234)
+                };
+                shell = ShellProvider.GenerateShell(monitor, true);
+                shell.Terminal = new NavigableTerminalEmulator(io, true);
+
+                Logger.Log(LogLevel.Info, "Monitor available in telnet mode on port {0}", 1234);
+                
+                shell.Quitted += Emulator.Exit;
+
+                monitor.Interaction = shell.Writer;
+                monitor.MachineChanged += emu => shell.SetPrompt(emu != null ? new Prompt(string.Format("({0}) ", emu), ConsoleColor.DarkYellow) : null);
+
+                shell.Terminal.PlainMode = false;
+
+                new System.Threading.Thread(x => shell.Start(true))
+                {
+                    IsBackground = true,
+                    Name = "Shell thread"
+                }.Start();
+
+                Emulator.BeforeExit += () =>
+                {
+                    Emulator.DisposeAll();
+                };
+
+                Console.Out.WriteLine("Wait!");
+                Emulator.WaitForExit();
+                Console.Out.WriteLine("Exit!");
+            }
+        }
     }
 }
--- tools/metrics_analyzer/metrics_visualizer/assets/helpers.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def save_fig(fig, fileName, options):    
-    if options.output:
-        output_path = os.path.join(options.output, fileName)
-    else:
-        output_path = fileName
-
-    fig.savefig(output_path)
