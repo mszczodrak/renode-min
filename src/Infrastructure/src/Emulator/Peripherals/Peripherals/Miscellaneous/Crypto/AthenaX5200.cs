a8d128ccbab15c35a4066785b2a91bc23701eff4
mszczodrak@gmail.com <mszczodrak@gmail.com>
2023-03-30 14:09:15 -0700

    remove crypto lib

--- src/Infrastructure/src/Emulator/Peripherals/Peripherals/Miscellaneous/Crypto/AthenaX5200.cs
+++ src/Infrastructure/src/Emulator/Peripherals/Peripherals/Miscellaneous/Crypto/AthenaX5200.cs
@@ -21,7 +21,6 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous.Crypto
             RegistersCollection = new DoubleWordRegisterCollection(this);
             rsaServiceProvider = new RSAServiceProvider(memoryManager);
             aesServiceProvider = new AESServiceProvider(memoryManager, machine.SystemBus);
-            msgAuthServiceProvider = new MessageAuthenticationServiceProvider(memoryManager, machine.SystemBus);
             dsaServiceProvider = new DSAServiceProvider(memoryManager, machine.SystemBus);
 
             Registers.CSR.Define(this)
@@ -78,11 +77,6 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous.Crypto
                 { JumpTable.DecryptCipherRSA, rsaServiceProvider.DecryptData },
                 { JumpTable.RunAES, aesServiceProvider.PerformAESOperation },
                 { JumpTable.RunAES_DMA, aesServiceProvider.PerformAESOperationDMA },
-                { JumpTable.RunGCM, msgAuthServiceProvider.PerformGCMMessageAuthentication },
-                { JumpTable.RunGCMNew, msgAuthServiceProvider.PerformGCMMessageAuthentication },
-                { JumpTable.RunSHA, msgAuthServiceProvider.PerformSHA },
-                { JumpTable.RunSHADMA, msgAuthServiceProvider.PerformSHADMA },
-                { JumpTable.RunHMACSHA, msgAuthServiceProvider.PerformHMACSHA },
                 { JumpTable.RunDSA_Sign, dsaServiceProvider.SignDMA },
             };
 
@@ -108,7 +102,6 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous.Crypto
         {
             memoryManager.ResetMemories();
             RegistersCollection.Reset();
-            msgAuthServiceProvider.Reset();
             rsaServiceProvider.Reset();
             aesServiceProvider.Reset();
             randomGenerator?.Reset();
@@ -165,7 +158,6 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous.Crypto
         private readonly IFlagRegisterField coreExecuteCommand;
         private readonly RSAServiceProvider rsaServiceProvider;
         private readonly AESServiceProvider aesServiceProvider;
-        private readonly MessageAuthenticationServiceProvider msgAuthServiceProvider;
         private readonly DSAServiceProvider dsaServiceProvider;
         
         private enum JumpTable
